521dc6a535ef0aba5ed5d93ec2c364050fdd07d5
mohamadjardaly-dowgroup <mohamad@dowgroup.com>
2024-09-19 14:59:44 +0300

    Add files via upload

---
+++ netag_co/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Netag Company',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Custom quotation template with company details and logo',
+    'depends': ['sale'],
+    'data': [
+        'view/quotaion_temp.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
