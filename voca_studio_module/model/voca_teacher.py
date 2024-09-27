7db824274d4b8e206ef60323d747be840015bad6
mohamadjardaly-dowgroup <mohamad@dowgroup.com>
2024-09-27 16:38:55 +0300

    Update voca_teacher.py

--- voca_studio_module/model/voca_teacher.py
+++ voca_studio_module/model/voca_teacher.py
@@ -6,11 +6,11 @@ class Teacher(models.Model):
     _name = 'voca.teacher'
     # _description = 'Portal'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name',related='instructor.name' )
 
     image_1920 = fields.Image(string="Image",store=True, readonly=False)  # image.mixin override
 
-    tag_ids = fields.Many2many('voca.teacher.tags', string='Tags' )
+    tag_ids = fields.Many2many('voca.teacher.tags', string='Tags',related='instructor.tag_ids'  )
 
 #     tag_ids = fields.Many2many(
 #     'voca.teacher.tags', 
@@ -22,7 +22,7 @@ class Teacher(models.Model):
 
     instructor = fields.Many2one('res.partner',string='Instructor')
 
-    experience = fields.Char(string='Experience' )
+    experience = fields.Char(string='Experience',related='instructor.experience'  )
 
 
 #     categories = fields.Many2many(
@@ -33,14 +33,14 @@ class Teacher(models.Model):
 #     string='Tags'
 # )
 
-    categories = fields.Many2many('voca.teacher.categories',string='Category' )
+    categories = fields.Many2many('voca.teacher.categories',string='Category',related='instructor.category_ids' )
 
     language = fields.Selection([
             ('en', 'English'),
             ('ar', 'Arabic'),
         ], string="Language")
 
-    about = fields.Char(string='About')
+    about = fields.Char(string='About',related='instructor.about')
     learning_bio = fields.Char(string='Learn bio')
     slot_ids = fields.One2many('voca.teacher.slots', 'slot_id', string='Direct subordinates')
 
