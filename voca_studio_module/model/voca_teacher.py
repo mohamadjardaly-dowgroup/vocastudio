f5740d0c1c0203ea9acc2a8783052be6b0ea337d
mohamadjardaly-dowgroup <mohamad@dowgroup.com>
2024-09-27 16:27:32 +0300

    Update voca_teacher.py

--- voca_studio_module/model/voca_teacher.py
+++ voca_studio_module/model/voca_teacher.py
@@ -6,7 +6,7 @@ class Teacher(models.Model):
     _name = 'voca.teacher'
     # _description = 'Portal'
 
-    name = fields.Char(string='Name',related='instructor.name')
+    name = fields.Char(string='Name')
 
     image_1920 = fields.Image(string="Image",store=True, readonly=False)  # image.mixin override
 
@@ -22,7 +22,7 @@ class Teacher(models.Model):
 
     instructor = fields.Many2one('res.partner',string='Instructor')
 
-    experience = fields.Char(string='Experience',related='instructor.experience' )
+    experience = fields.Char(string='Experience' )
 
 
 #     categories = fields.Many2many(
@@ -40,7 +40,7 @@ class Teacher(models.Model):
             ('ar', 'Arabic'),
         ], string="Language")
 
-    about = fields.Char(string='About',related='instructor.about')
+    about = fields.Char(string='About')
     learning_bio = fields.Char(string='Learn bio')
     slot_ids = fields.One2many('voca.teacher.slots', 'slot_id', string='Direct subordinates')
 
