ba5a5fbc80ca041470094cad2fa365edb4461008
mohamadjardaly-dowgroup <mohamad@dowgroup.com>
2024-09-27 15:21:18 +0300

    Add files via upload

--- voca_studio_module/__manifest__.py
+++ voca_studio_module/__manifest__.py
@@ -11,12 +11,17 @@
     "depends": ["base", 'website','website_slides' ],
     "data": [
         'security/ir.model.access.csv',
+        'data/teacher_website_menu.xml',
         'view/voca_teacher_view.xml',
         'view/voca_teacher_tags_view.xml',
         'view/voca_teacher_categories_view.xml',
         'view/website_template.xml',
         'view/slide_channel_views.xml',
         'view/website_slides_templates_course.xml',
+        'view/appointment_template.xml',
+        'view/auth_signup_template.xml',
+        'view/portal_template.xml',
+        # 'view/res_partner_views.xml',
 
     ],
 
