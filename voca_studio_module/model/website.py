e64c5a66972ea822bc6feee142a70909fcf66496
mohamadjardaly-dowgroup <mohamad@dowgroup.com>
2024-11-07 13:52:28 +0300

    Update website.py

--- voca_studio_module/model/website.py
+++ voca_studio_module/model/website.py
@@ -17,7 +17,6 @@ class Website(models.Model):
         first_order_line = sale_order.order_line[:1]  # This fetches only the first order line
 
         if request and request.session :
-            _logger.info('hooooo', request.session)
 
             sale_order.write({'package_id': request.session.get('package_id').get('id')})
             selected_bookings = request.session.get('selected_bookings')
@@ -43,7 +42,7 @@ class Website(models.Model):
                             'price_unit': request.session.get('package_id').get('price'),
                             'booking_ids': [(6, 0, booking_lines.ids)]})
                     else:
-
+                        _logger.info('else', request.session,booking_dates, booking_lines, first_order_line)
                         booking_master_lines.write({'status': 'booked'})
                         first_order_line.write({
                             'price_unit': request.session.get('package_id').get('price'),
