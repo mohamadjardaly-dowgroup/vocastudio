db7f1accc53223ee7ba448bae6b276d33de50456
mohamadjardaly-dowgroup <mohamad@dowgroup.com>
2024-11-20 13:39:08 +0300

    Add files via upload

---
+++ override_add_to_cart/static/src/js/website_sale_utils.js
@@ -0,0 +1,22 @@
+/** @odoo-module **/
+
+import { cartHandlerMixin } from '@website_sale/js/website_sale_utils';
+import { patch } from "@web/core/utils/patch";
+import wUtils from '@website/js/utils';
+
+
+patch(cartHandlerMixin, {
+
+     addToCart(params) {
+     const data =  super.addToCart(...arguments);
+
+    console.info('hello');
+        if (this.isBuyNow) {
+            params.express = true;
+        } else if (this.stayOnPageOption) {
+            this._addToCartInPage(params);
+            return wUtils.sendRequest('/shop/checkout', {"express": "1"});
+        }
+        return wUtils.sendRequest('/shop/cart/update', params);
+    },
+});
