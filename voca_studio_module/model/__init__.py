ad928060ae57191a43cdfa7256ee3a7ade95c2f7
samiha996 <144477577+samiha996@users.noreply.github.com>
2024-12-30 09:25:38 +0200

    Update __init__.py

--- voca_studio_module/model/__init__.py
+++ voca_studio_module/model/__init__.py
@@ -7,6 +7,8 @@ from . import res_partner
 from . import time_slots
 from . import voca_techer_packaging_lines
 from . import sale_order
-from . import website
+# from . import website
 from . import master_classes
 from . import product_product
+# samiha
+from . import product_template
