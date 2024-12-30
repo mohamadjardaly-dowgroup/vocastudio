3292e14b974fbb9f9639b930cd10f7139029d3ca
samiha996 <144477577+samiha996@users.noreply.github.com>
2024-12-30 09:30:25 +0200

    Update sale_order.py

--- voca_studio_module/model/sale_order.py
+++ voca_studio_module/model/sale_order.py
@@ -11,6 +11,28 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     package_id = fields.Many2one('voca.teacher.packaging.lines',string='Teacher package')
+    
+    #samiha##########################################################
+    def action_confirm(self):
+        print("Sale Order Confirmed: ...................", self.name)
+        res = super(SaleOrder, self).action_confirm()
+        
+        for order in self:
+            for line in order.order_line:
+                if line.booking_ids:
+                    print("I am inside if line.booking ids ////////")
+                    for booking_id in line.booking_ids:
+                        print('Updating Booking ID:', booking_id.id)
+                        booking_id.write({'status': 'booked'})
+                        print("the status of booking id is ...............",booking_id.status)
+                        
+                elif line.booking_master_ids :
+                    print("I am inside if booking_master_ids ////////")
+                    for booking_master_id in line.booking_master_ids:
+                        print('Updating Booking ID:', booking_master_id.id)
+                        booking_master_id.write({'status': 'booked'})
+                        print("the status of booking id is ...............",booking_master_id.status)
+        return res
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
