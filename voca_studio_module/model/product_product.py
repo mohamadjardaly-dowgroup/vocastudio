c556b833446e07b0c01066ef58bd66a580f99a05
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-01-14 09:20:40 +0200

    Update product_product.py

--- voca_studio_module/model/product_product.py
+++ voca_studio_module/model/product_product.py
@@ -6,6 +6,9 @@ class Product(models.Model):
     _inherit = 'product.product'
 
     is_master = fields.Boolean(string='Is Master')
+    
+    #samiha##########################################################
+    master_class_id = fields.Many2one('master.classes', string='Master Class')
 
 class Product(models.Model):
     _inherit = 'product.template'
