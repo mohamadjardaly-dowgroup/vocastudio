ebe67546158b485c309d4388307a5cd4dbe0e956
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-01-14 09:42:11 +0200

    Update __manifest__.py

--- voca_studio_module/__manifest__.py
+++ voca_studio_module/__manifest__.py
@@ -39,7 +39,7 @@
             #
             'voca_studio_module/static/src/js/bookShow.js',
             'voca_studio_module/static/src/js/selectSeat.js',
-            'voca_studio_module/static/src/js/website_sale_utils.js',
+            # 'voca_studio_module/static/src/js/website_sale_utils.js',
 
             # 'voca_studio_module/static/src/css/show_booking_management.css',
         ],
