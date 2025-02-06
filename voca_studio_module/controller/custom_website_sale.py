569026169555a714d3af96243f55b89c272ac953
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-02-06 13:10:18 +0200

    Update custom_website_sale.py

--- voca_studio_module/controller/custom_website_sale.py
+++ voca_studio_module/controller/custom_website_sale.py
@@ -5,7 +5,7 @@ import pytz
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.addons.website.controllers.main import QueryURL
 from math import ceil
-from odoo import http
+from odoo import fields, http
 from odoo.http import request
 from odoo.exceptions import UserError
 from odoo.tools import format_amount
