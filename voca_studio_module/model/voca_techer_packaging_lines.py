38ca31c691a5426cba310ef0e7bfbd4a81316908
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-02-19 14:53:19 +0200

    Update voca_techer_packaging_lines.py

--- voca_studio_module/model/voca_techer_packaging_lines.py
+++ voca_studio_module/model/voca_techer_packaging_lines.py
@@ -18,10 +18,22 @@ class TeacherPackaging(models.Model):
     price = fields.Monetary('Price', currency_field="currency_id", required=True,store=True,readonly=False)
     currency_id = fields.Many2one('res.currency', string='Currency', required=True, default=lambda self: self.env.company.currency_id.id)
     
+    discount=fields.Float('Discount (%)', digits='Discount', default=0.0)
+    total = fields.Monetary('Total', currency_field="currency_id", compute='_compute_total', store=True)
+    
     package_time = fields.Char(string='Time')
 
     order_line_ids = fields.One2many('sale.order', 'package_id', string='Sale Lines')
     product_id = fields.Many2one('product.product', string='Product',related="package_id.product_id", readonly=True)
+    
+    @api.depends('price', 'discount')
+    def _compute_total(self):
+        for record in self:
+            record.total = record.price *  (1 - (record.discount or 0.0) / 100)
+            print("record.total ...........", record.total)
+
+
+
 
 
 
