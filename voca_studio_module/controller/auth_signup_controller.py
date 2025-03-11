d64321092a7e9b19c2ad421e96e9e6fe641ca9f3
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-03-11 11:13:11 +0200

    Update auth_signup_controller.py

--- voca_studio_module/controller/auth_signup_controller.py
+++ voca_studio_module/controller/auth_signup_controller.py
@@ -35,7 +35,7 @@ class VocaAuthSignupHome(AuthSignupHome):
         """Extend context with additional fields for signup."""
         SIGN_UP_REQUEST_PARAMS_CUSTOM = [
             'first_name', 'nickname', 'phone', 'gender', 'birthday', 'role',
-            'experience', 'about', 'is_teacher', 'attachment_ids','instrument'
+            'experience', 'about', 'is_teacher', 'attachment_ids','instrument','level'
         ]
         qcontext = super(VocaAuthSignupHome, self).get_auth_signup_qcontext()
         qcontext.update({k: v for (k, v) in request.params.items() if k in SIGN_UP_REQUEST_PARAMS_CUSTOM})
@@ -82,6 +82,18 @@ class VocaAuthSignupHome(AuthSignupHome):
             'login', 'name', 'password', 'first_name', 'nickname', 'phone', 'gender', 
             'birthday', 'role', 'experience', 'about', 'is_teacher','instrument'
         )}
+        if values.get('role') == 'student':
+            print("inside the if role is student .............. ")
+            level = qcontext.get('level')
+            print("level is .............. ",level)
+            if level:
+                level_tag = request.env['res.partner.category'].sudo().search([('name', '=', level)], limit=1)
+                print("level_tag is........... ",level_tag)
+                if not level_tag:
+                    level_tag = request.env['res.partner.category'].sudo().create({'name': level})
+                    print("level_tag is  if not......... ",level_tag)
+                values['category_id'] = [(4, level_tag.id)]  # Assign the level as a tag
+                print("values['category_id'] is ........ ",values['category_id'])
         if values.get('role') == 'teacher' and not values.get('instrument'):
             raise UserError(_("Instrument field is required for teachers. Please specify the instrument you teach."))
         # Validate email
