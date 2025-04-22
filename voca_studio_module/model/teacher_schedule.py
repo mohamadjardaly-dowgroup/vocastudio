bd79899ae3e3a7a428cd86c9c50f348c939e138a
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-04-22 10:32:13 +0300

    Update teacher_schedule.py

--- voca_studio_module/model/teacher_schedule.py
+++ voca_studio_module/model/teacher_schedule.py
@@ -12,7 +12,7 @@ class VocaTeacherSchedule(models.Model):
         ('0', 'Monday'), ('1', 'Tuesday'), ('2', 'Wednesday'),
         ('3', 'Thursday'), ('4', 'Friday'), ('5', 'Saturday'), ('6', 'Sunday')
     ], string='Day of Week', required=True)
-    time_from = fields.Float(string='From (24h)', required=True)
-    time_to = fields.Float(string='To (24h)', required=True)
+    time_from = fields.Float(string='From (24h, utc)', required=True)
+    time_to = fields.Float(string='To (24h, utc)', required=True)
 
     
