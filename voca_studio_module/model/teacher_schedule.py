08b68ae9a8cc983388346b378787408921d68e6e
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-04-22 10:27:51 +0300

    Add files via upload

---
+++ voca_studio_module/model/teacher_schedule.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api
+from datetime import datetime, timedelta
+import pytz
+
+
+class VocaTeacherSchedule(models.Model):
+    _name = 'voca.teacher.schedule'
+    _description = "Teacher Weekly Schedule"
+
+    teacher_id = fields.Many2one('voca.teacher', required=True)
+    weekday = fields.Selection([
+        ('0', 'Monday'), ('1', 'Tuesday'), ('2', 'Wednesday'),
+        ('3', 'Thursday'), ('4', 'Friday'), ('5', 'Saturday'), ('6', 'Sunday')
+    ], string='Day of Week', required=True)
+    time_from = fields.Float(string='From (24h)', required=True)
+    time_to = fields.Float(string='To (24h)', required=True)
+
+    
