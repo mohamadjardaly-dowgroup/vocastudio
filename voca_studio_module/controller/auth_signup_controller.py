eecc18e87ca4d8cdb2dd6a9b2abe5797e3a06f1b
samiha996 <144477577+samiha996@users.noreply.github.com>
2025-04-29 14:22:38 +0300

    Update auth_signup_controller.py

--- voca_studio_module/controller/auth_signup_controller.py
+++ voca_studio_module/controller/auth_signup_controller.py
@@ -178,6 +178,7 @@ class VocaAuthSignupHome(AuthSignupHome):
             })
         """Handle web signup with role-based validations."""
         qcontext = self.get_auth_signup_qcontext()
+        qcontext['role'] = role
         if not qcontext.get('token') and not qcontext.get('signup_enabled'):
             raise werkzeug.exceptions.NotFound()
 
